4a67fffbc963c3cbdacd6148d7b23ff2b7842111
biswaranjan.nayak <Krishna@6688>
2024-06-27 10:03:11 +0530

    aaaa

---
+++ dist/index.js
@@ -0,0 +1,37 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const node_json_db_1 = require("node-json-db");
+var db = new node_json_db_1.JsonDB(new node_json_db_1.Config("db.json", true, false, '/'));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 8080;
+app.get('/', (_req, res) => {
+    return res.send('Express Typescript on Vercel');
+});
+app.get('/ping', (_req, res) => {
+    return res.send('pong 🏓');
+});
+app.get('/posts', async (_req, res) => {
+    db.getData("/posts").then((val) => {
+        return res.send(JSON.stringify(val));
+    }).catch((err) => {
+        return res.send(JSON.stringify(err));
+    });
+});
+app.get('/comments', async (_req, res) => {
+    // var data = await db.getData("/comments");
+    // return res.send(JSON.stringify(data))
+    return res.send('hello');
+});
+app.get('/profile', (_req, res) => {
+    // var data = await db.getData("/profile");
+    return res.send('hello');
+});
+app.listen(port, () => {
+    return console.log(`Server is listening on ${port}`);
+});
+// module.exports = app;
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response } from 'express'
+import { Config, JsonDB } from 'node-json-db';
+
+var db = new JsonDB(new Config("db.json", true, false, '/'));
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (_req: Request, res: Response) => {
+  return res.send('Express Typescript on Vercel')
+})
+
+app.get('/ping', (_req: Request, res: Response) => {
+  return res.send('pong 🏓')
+})
+
+app.get('/posts', async (_req: Request, res: Response) => {
+  db.getData("/posts").then((val: any) => {
+    return res.send(JSON.stringify(val));
+  }).catch((err: any) => {
+    return res.send(JSON.stringify(err));
+  });
+  
+})
+
+app.get('/comments', async (_req: Request, res: Response) => {
+  // var data = await db.getData("/comments");
+  // return res.send(JSON.stringify(data))
+  return res.send('hello')
+})
+
+app.get('/profile', (_req: Request, res: Response) => {
+  // var data = await db.getData("/profile");
+  return res.send('hello')
+})
+
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`)
+})
+
+// module.exports = app;
